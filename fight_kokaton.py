3d5272ace1532011640d4209949fdca390198e0c
c0a22021 <c0a22021b6@edu.teu.ac.jp>
2023-12-05 14:36:51 +0900

    練習2 : 喜びエフェクト

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -163,13 +163,14 @@ def main():
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
                 pg.display.update()
-                time.sleep(2)
+                time.sleep(1)
                 return
 
         if beam is not None and bomb is not None:
             if beam.rct.colliderect(bomb.rct):
                 beam = None
                 bomb = None
+                bird.change_img(6, screen)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
